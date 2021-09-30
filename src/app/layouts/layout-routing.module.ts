f7bfcc2b9bd544d4aa4085bf0e71e81cd50474f9
Francis Judd <39587456+francisjudd@users.noreply.github.com>
2021-09-30 02:38:16 +0100

    Fix routing

--- src/app/layouts/layout-routing.module.ts
+++ src/app/layouts/layout-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule)
       }
     ]
   }
