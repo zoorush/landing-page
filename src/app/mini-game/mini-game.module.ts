263a580d36f8052578a63aff71dd66c9890dd656
Francis Judd <39587456+francisjudd@users.noreply.github.com>
2021-10-27 00:08:41 +0100

    ***Animations***

--- src/app/mini-game/mini-game.module.ts
+++ src/app/mini-game/mini-game.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MiniGameRoutingModule } from './mini-game-routing.module';
 import { MiniGameComponent } from './mini-game.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   exports: [MiniGameComponent],
@@ -14,6 +15,8 @@ import { SharedModule } from '../shared/shared.module';
     FlexLayoutModule,
     MiniGameRoutingModule,
     SharedModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class MiniGameModule {}
